ad9f6427c8b81e40478f554324550853b669ed09
Sundar Soren <sundarsoren100@gmail.com>
2023-03-29 02:48:36 +0530

    changed

--- frontend/src/utils/newRequest.js
+++ frontend/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const URL = "http://localhost:8000/";
-// const URL = "https://cipher-profile.vercel.app/";
+// const URL = "http://localhost:8000/";
+const URL = "https://cipher-profile.vercel.app/";
 
 const newRequest = axios.create({
   headers: {
